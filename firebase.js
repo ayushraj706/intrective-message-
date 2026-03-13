72f17e6c23d49056e2d6cecef475b7585d0a23e6
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-13 17:56:26 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCqWVSgULjZtgfOqVX3CBmOonxkr2UB7g",
+  authDomain: "whatsapp-950a8.firebaseapp.com",
+  projectId: "whatsapp-950a8",
+  storageBucket: "whatsapp-950a8.firebasestorage.app",
+  messagingSenderId: "526342181957",
+  appId: "1:526342181957:web:0e71810f3ccbb297413f2c"
+};
+
+// Initialize Firebase (SSR Safe)
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
