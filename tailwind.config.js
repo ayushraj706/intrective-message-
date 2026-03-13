17c4885271a862e7b45195d4ba71a5948594c6a9
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-13 19:01:34 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,10 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // सिस्टम और मैनुअल टॉगल के लिए
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
