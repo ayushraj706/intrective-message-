2a5a366c8baaee84249f9964d78f1a1b3261f799
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-13 18:19:32 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // सिस्टम और मैनुअल टॉगल के लिए
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
