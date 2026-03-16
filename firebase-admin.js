bf6b57559a81d2b515b773a4d7ba8f32998205eb
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-17 02:29:02 +0530

    Update firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,12 +1,15 @@
-// firebase-admin.js (Root folder mein)
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
-  });
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
+    });
+    console.log("🚀 Firebase Admin Initialized");
+  } catch (error) {
+    console.error("🔥 Firebase Admin Init Error:", error);
+  }
 }
 
 const db = admin.firestore();
 export { db };
-
