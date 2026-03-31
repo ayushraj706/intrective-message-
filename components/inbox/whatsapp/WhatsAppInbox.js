b1062bde0becb955e07535d73a9dca9297746b9b
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-31 21:40:34 +0530

    Update WhatsAppInbox.js

--- components/inbox/whatsapp/WhatsAppInbox.js
+++ components/inbox/whatsapp/WhatsAppInbox.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db, auth } from '../firebase'; 
+import { db, auth } from '../../../firebase'; 
 import { collection, query, onSnapshot, orderBy } from 'firebase/firestore';
 import axios from 'axios';
 import { toast } from 'sonner';
